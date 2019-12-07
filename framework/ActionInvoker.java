9d2199c4d47fb0db48641a6d10e7a200aa8e31ea
Chesare22 <Chesare22@github.com>
2019-12-06 20:19:35 -0600

    Framework listo

---
+++ framework/ActionInvoker.java
@@ -0,0 +1,42 @@
+package framework;
+
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+
+public class ActionInvoker {
+    private static final ActionInvoker singleton = new ActionInvoker() ;
+
+    private final HashMap<String, Action> actions = new HashMap<>();
+
+    private ActionInvoker() {
+        XMLReader reader = new XMLReader();
+        Action[] actionsArray = new Action[0];
+        try {
+            actionsArray = reader.readActions("config.xml");
+        } catch (InvocationTargetException | SAXException | ParserConfigurationException | IllegalAccessException | IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        for (Action action : actionsArray) {
+            actions.put(action.getActionName(), action);
+        }
+    }
+
+    public static ActionInvoker getInstance() {
+        return singleton;
+    }
+
+    public Object invokeAction(String actionName, Event event) {
+        try {
+            return actions.get(actionName).callMethod(event);
+        } catch (InvocationTargetException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
--- framework/example/RunExample.java
+++ framework/example/RunExample.java
@@ -1,23 +1,11 @@
 package framework.example;
 
-import framework.Action;
+import framework.ActionInvoker;
 import framework.Event;
-import framework.XMLReader;
-import org.xml.sax.SAXException;
-
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
 
 public class RunExample {
     public static void main(String[] args) {
-        XMLReader reader = new XMLReader();
-        Action[] actions;
-        try {
-            actions = reader.readActions("config.xml");
-            actions[0].callMethod(new Event("Rico", 2));
-        } catch (ParserConfigurationException | InvocationTargetException | IllegalAccessException | SAXException | IOException e) {
-            e.printStackTrace();
-        }
+        ActionInvoker invoker = ActionInvoker.getInstance();
+        invoker.invokeAction("add_vote", new Event("Rico", 2));
     }
 }
